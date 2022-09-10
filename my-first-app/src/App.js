b6e4090e546aaff4c188da1a36fab47dd0b480a1
VNATAS3 <sathishkumar.natarajan1@pearson.com>
2022-09-10 22:30:59 +0530

    folder structure of react

---
+++ my-first-app/src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <p>here the execution cycle structur is App.js-->index.js-->index.html</p>
+    </div>
+  );
+}
+
+export default App;
